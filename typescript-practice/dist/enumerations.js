553a3842d8a78191da43a0112a1dcb84fc992474
Anmol-Singh-Chehal <devanmolsinghchehal@gmail.com>
2025-07-08 18:49:01 +0530

    Delete typescript-practice directory

--- typescript-practice/dist/enumerations.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-let move = 0 /* Direction.up */;
-move = 1 /* Direction.down */;
-;
-;
-let myValue = "This is first" /* more.first */;
-console.log(myValue);
